80de0b120f395776ad98a1f6f29f75411ecb2f2d
JGY <JGY@172-14-21-210.lightspeed.moblal.sbcglobal.net>
2026-04-28 11:32:48 +0900

    fix: ES module export default → CommonJS module.exports로 변경 (Vercel 404 수정)

--- api/search.js
+++ api/search.js
@@ -8,7 +8,7 @@ async function fetchJson(url, headers) {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   const clientId = process.env.NAVER_CLIENT_ID;
